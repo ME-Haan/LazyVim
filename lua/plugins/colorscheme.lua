be2c67a3dcfbb04554fddb60c46caeccc81d7d1d
MH <martinhaan@hotmail.com>
2023-07-05 21:17:58 +0200

    basic setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  { "rebelot/kanagawa.nvim", lazy = true },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
