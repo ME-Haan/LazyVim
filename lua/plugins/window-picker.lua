e98bcf6a0402e56aa0386990e7e638ce6034056f
MH <martinhaan@hotmail.com>
2023-07-11 08:26:12 +0200

    changes to windowpicker

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -31,17 +31,6 @@ return {
           -- if the file type is one of following, the window will be ignored
           buftype = { "terminal" },
         },
-
-        -- filter using window options
-        wo = {},
-
-        -- if the file path contains one of following names, the window
-        -- will be ignored
-        file_path_contains = {},
-
-        -- if the file name contains one of following names, the window will be
-        -- ignored
-        file_name_contains = {},
       },
     },
     -- config = function()
