e8bf787273033c134657615e7dc2975556967ff2
Martin <martinhaan91+git@gmail.com>
2023-10-14 20:16:34 +0200

    removed extras from config, easier config for window picker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,12 @@ return {
   -- add gruvbox
   -- { "ellisonleao/gruvbox.nvim", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -1,40 +1,50 @@
 return {
   {
-    "s1n7ax/nvim-window-picker",
-    name = "window-picker",
-    event = "VeryLazy",
-    version = "2.*",
-    opts = {
-      picker_config = { statusline_winbar_picker = { use_winbar = "smart" } },
-      filter_rules = {
-        -- when there is only one window available to pick from, use that window
-        -- without prompting the user to select
-        autoselect_one = true,
-
-        -- whether you want to include the window you are currently on to window
-        -- selection or not
-        include_current_win = false,
-
-        -- filter using buffer options
-        bo = {
-          -- if the file type is one of following, the window will be ignored
-          filetype = {
-            "NvimTree",
-            "neo-tree",
-            "neo-tree-popup",
-            "neo-tree-buffers",
-            "neo-tree-git-status-source",
-            "notify",
-            "quickfix",
-          },
-
-          -- if the file type is one of following, the window will be ignored
-          buftype = { "terminal" },
-        },
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "s1n7ax/nvim-window-picker",
+      opts = {},
     },
-    -- config = function()
-    --   require("window-picker").setup()
-    -- end,
   },
 }
+
+-- return {
+--   {
+--     "s1n7ax/nvim-window-picker",
+--     name = "window-picker",
+--     event = "VeryLazy",
+--     version = "2.*",
+--     opts = {
+--       picker_config = { statusline_winbar_picker = { use_winbar = "smart" } },
+--       filter_rules = {
+--         -- when there is only one window available to pick from, use that window
+--         -- without prompting the user to select
+--         autoselect_one = true,
+--
+--         -- whether you want to include the window you are currently on to window
+--         -- selection or not
+--         include_current_win = false,
+--
+--         -- filter using buffer options
+--         bo = {
+--           -- if the file type is one of following, the window will be ignored
+--           filetype = {
+--             "NvimTree",
+--             "neo-tree",
+--             "neo-tree-popup",
+--             "neo-tree-buffers",
+--             "neo-tree-git-status-source",
+--             "notify",
+--             "quickfix",
+--           },
+--
+--           -- if the file type is one of following, the window will be ignored
+--           buftype = { "terminal" },
+--         },
+--       },
+--     },
+--     -- config = function()
+--     --   require("window-picker").setup()
+--     -- end,
+--   },
+-- }
